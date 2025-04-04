e24ad8c0a0eeba270ced07a6c667c134f924a77f
JAYADHEEP356 <jayadheep356@gmail.com>
2025-04-04 11:00:26 +0530

    newcode

---
+++ src/main/java/com/example/jwt_generation/Service/JwtService.java
@@ -0,0 +1,15 @@
+package com.example.jwt_generation.Service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String generateToken(){
+
+        return "";
+    }
+
+
+}
