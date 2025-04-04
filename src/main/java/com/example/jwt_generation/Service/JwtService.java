751a89b320f49c55d7480382137ac033d3c9b49d
JAYADHEEP356 <jayadheep356@gmail.com>
2025-04-04 20:31:05 +0530

    new_changes

--- src/main/java/com/example/jwt_generation/Service/JwtService.java
+++ src/main/java/com/example/jwt_generation/Service/JwtService.java
@@ -1,14 +1,30 @@
 package com.example.jwt_generation.Service;
 
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
 
-    public String generateToken(){
+    public String generateToken(String username){
 
-        return "";
+         Map<String,Object> map = new HashMap<>(); //for custom claims
+
+         return Jwts.builder()
+                 .claims()
+                 .add(map)
+                 .subject(username)
+                 .issuedAt(new Date(System.currentTimeMillis()))
+                 .expiration(new Date(System.currentTimeMillis()*60*60*30))
+                 .and()
+                 .signWith(getkey())
+                 .compact();
+
     }
 
 
